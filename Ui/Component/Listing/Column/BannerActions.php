d8f63155d6b6903c2130a1085673252a9bbb039e
Perry <perry.mark@zeald.com>
2017-10-14 22:00:30 +0800

    Added date and action columns

---
+++ Ui/Component/Listing/Column/BannerActions.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Prymag\BannerSlider\Ui\Component\Listing\Column;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\UrlInterface;
+
+class BannerActions extends Column {
+
+    const EDIT_URL = 'prymag_bannerslider/banners/edit';
+    const DELETE_URL = 'prymag_bannerslider/banners/delete';
+
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                $name = $this->getData('name');
+                if (isset($item['banner_id'])) {
+                    $item[$name]['edit'] = [
+                        'href' => $this->urlBuilder->getUrl(self::EDIT_URL, ['banner_id' => $item['banner_id']]),
+                        'label' => __('Edit')
+                    ];
+                    $item[$name]['delete'] = [
+                        'href' => $this->urlBuilder->getUrl(self::DELETE_URL, ['banner_id' => $item['banner_id']]),
+                        'label' => __('Delete'),
+                        'confirm' => [
+                            'title' => __('Delete "${ $.$data.title }"'),
+                            'message' => __('Are you sure you wan\'t to delete a "${ $.$data.title }" record?')
+                        ]
+                    ];
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+
+}

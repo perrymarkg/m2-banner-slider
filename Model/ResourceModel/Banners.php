86920dedec4b330032b82edcf0d8027ead1a46c7
Perry <perry.mark@zeald.com>
2017-10-14 20:57:12 +0800

    Model/Resource re-structuring

---
+++ Model/ResourceModel/Banners.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Prymag\BannerSlider\Model\ResourceModel;
+
+class Banners extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb {
+    
+    /**
+     * Define table for this resource and the primary key
+     *
+     * @return void
+     */
+    protected function _construct(){
+        $this->_init('prymag_banners', 'banner_id');
+    }
+    
+}

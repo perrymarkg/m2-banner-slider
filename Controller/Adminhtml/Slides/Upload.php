2b7f9f8c7df9bd4bd5e65cb37080eb5e53723670
Perry <perry.mark@zeald.com>
2017-10-18 13:33:41 +0800

    upload controller for images from fileUploader

---
+++ Controller/Adminhtml/Slides/Upload.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ * Replica of Magento\Catalog\Controller\Adminhtml\Category\Image\Upload
+ */
+namespace Prymag\BannerSlider\Controller\Adminhtml\Slides;
+
+use Magento\Framework\Controller\ResultFactory;
+
+/**
+ * Class Upload
+ */
+class Upload extends \Magento\Backend\App\Action
+{
+    /**
+     * Image uploader
+     *
+     * @var \Magento\Catalog\Model\ImageUploader
+     */
+    protected $imageUploader;
+
+    /**
+     * Upload constructor.
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Catalog\Model\ImageUploader $imageUploader
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Prymag\BannerSlider\Model\Image\ImageUploader $imageUploader
+    ) {
+        parent::__construct($context);
+        $this->imageUploader = $imageUploader;
+    }
+
+
+    /**
+     * Upload file controller action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $imageId = $this->_request->getParam('param_name', 'image');
+
+        try {
+            $result = $this->imageUploader->saveFileToTmpDir($imageId);
+
+            $result['cookie'] = [
+                'name' => $this->_getSession()->getName(),
+                'value' => $this->_getSession()->getSessionId(),
+                'lifetime' => $this->_getSession()->getCookieLifetime(),
+                'path' => $this->_getSession()->getCookiePath(),
+                'domain' => $this->_getSession()->getCookieDomain(),
+            ];
+        } catch (\Exception $e) {
+            $result = ['error' => $e->getMessage(), 'errorcode' => $e->getCode()];
+        }
+        return $this->resultFactory->create(ResultFactory::TYPE_JSON)->setData($result);
+    }
+}

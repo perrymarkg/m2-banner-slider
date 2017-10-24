6861540634337d3c503a581731486efad4c4ce4e
Perry <perry.mark@zeald.com>
2017-10-24 09:46:52 +0800

    data structure sample for slides

--- Ui/DataProvider/BannerDataProvider.php
+++ Ui/DataProvider/BannerDataProvider.php
@@ -4,21 +4,28 @@ namespace Prymag\BannerSlider\Ui\DataProvider;
 
 use Prymag\BannerSlider\Model\ResourceModel\Banners\CollectionFactory;
 use \Magento\Ui\DataProvider\Modifier\PoolInterface;
+use Magento\Framework\App\Request\DataPersistorInterface;
 
 class BannerDataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider {
 
     private $pool;
+
+    protected $loadedData;
+
+    protected $dataPersistor;
     
     public function __construct(
         $name,
         $primaryFieldName,
         $requestFieldName,
         CollectionFactory $bannersCollectionFactory,
+        DataPersistorInterface $dataPersistor,
         PoolInterface $pool,
         array $meta = [],
         array $data = []
     ) {
         $this->collection = $bannersCollectionFactory->create();
+        $this->dataPersistor = $dataPersistor;
         $this->pool = $pool; // This will be injected via etc/adminhtml/di.xml
         parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
         
@@ -29,11 +36,43 @@ class BannerDataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider {
      */
     public function getData()
     {
-        /** @var ModifierInterface $modifier */
-        foreach ($this->pool->getModifiersInstances() as $modifier) {
-            $this->data = $modifier->modifyData($this->data);
+                
+        if (isset($this->loadedData)) {
+            return $this->loadedData;
         }
-        return $this->data;
+
+        $items = $this->collection->getItems();
+
+        foreach ($items as $banner) {
+            // $banner->getId() will automatically map to the idfield based on what is defined in it's resource model, no need to use getBannerId(). ?
+            $this->loadedData[$banner->getId()] = $banner->getData();
+        }
+
+        $data = $this->dataPersistor->get('prymag_banner'); 
+        if (!empty($data)) {
+            $banner = $this->collection->getNewEmptyItem();
+            $banner->setData($data);
+            $this->loadedData[$banner->getId()] = $banner->getData();
+            $this->dataPersistor->clear('prymag_banner');
+        }
+
+        // Dummy data format
+        
+        $this->loadedData[$banner->getId()]['slides']['banner_slides_dynamic_row'] = array(
+            array(
+                'id' => '1',
+                'title' => 'Test Title',
+                'position' => '0'
+            ),
+            array(
+                'id' => '2',
+                'title' => 'Test Title 2',
+                'position' => '0'
+            )
+        );
+       
+
+        return $this->loadedData;
     }
 
     /**

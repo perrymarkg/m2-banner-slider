a0b75e769d86fe3fd9eaabbbbafa8583e7af08be
Perry <perry.mark@zeald.com>
2017-10-24 09:45:21 +0800

    save action for banners

---
+++ Controller/Adminhtml/Banners/Save.php
@@ -0,0 +1,54 @@
+<?php 
+
+namespace Prymag\BannerSlider\Controller\Adminhtml\Banners;
+
+class Save extends \Magento\Backend\App\Action {
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\App\Request\DataPersistorInterface $dataPersistor
+    )
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        $this->dataPersistor = $dataPersistor;
+        parent::__construct($context);
+    }
+
+    public function execute(){
+        
+        $resultRedirect = $this->resultRedirectFactory->create();
+        
+        $data = $this->getRequest()->getPostValue();
+        var_dump($data);
+        die();
+        if ($data) {
+
+            $id = $this->getRequest()->getParam('banner_id');
+            $model = $this->_objectManager->create('Prymag\BannerSlider\Model\Banners')->load($id);
+
+            if (!$model->getBannerId() && $id) {
+                $this->messageManager->addError(__('This banner no longer exists.'));
+                return $resultRedirect->setPath('*/*/');
+            }
+
+            $model->setData($data);
+
+            try {
+                
+                $model->save();
+                $this->messageManager->addSuccess(__('Banner saved successfuly.'));
+                $this->dataPersistor->clear('prymag_banner');  
+
+                return $resultRedirect->setPath('*/*/edit', ['banner_id' => $model->getBannerId()]);
+            } catch (LocalizedException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (\Exception $e) {
+                $this->messageManager->addException($e, __('Something went wrong while saving the banner'));
+            }
+        }
+
+        return $resultRedirect->setPath('*/*/');
+    }
+
+}

96e12ca8c61952dec6090f9701b0f0ae85db2068
Perry <perry.mark@zeald.com>
2017-10-31 12:35:31 +0800

    build slides data for banner

--- Ui/DataProvider/BannerDataProvider.php
+++ Ui/DataProvider/BannerDataProvider.php
@@ -3,10 +3,12 @@
 namespace Prymag\BannerSlider\Ui\DataProvider;
 
 use Prymag\BannerSlider\Model\ResourceModel\Banners\CollectionFactory;
+use Prymag\BannerSlider\Model\ResourceModel\Banners\BannerSlidesCollectionFactory;
 use Prymag\BannerSlider\Model\BannersFactory;
 use \Magento\Ui\DataProvider\Modifier\PoolInterface;
 use Magento\Framework\App\Request\DataPersistorInterface;
 use Magento\Framework\App\RequestInterface;
+
 
 class BannerDataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider {
 
@@ -20,13 +22,14 @@ class BannerDataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider {
 
     protected $bannersFactory;
 
-    protected $bannersCollectionFactory;
+    protected $bannerSlideCollectionFactory;
     
     public function __construct(
         $name,
         $primaryFieldName,
         $requestFieldName,
         CollectionFactory $bannersCollectionFactory,
+        BannerSlidesCollectionFactory $bannerSlideCollectionFactory,
         DataPersistorInterface $dataPersistor,
         PoolInterface $pool,
         RequestInterface $request,
@@ -37,7 +40,7 @@ class BannerDataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider {
         // Collection is required as definend in \Magento\Ui\DataProvider\AbstractDataProvider, point it to the appropriate collection
         $this->collection = $bannersCollectionFactory->create(); 
         // create a new collectionFactory to use later on.
-        $this->bannersCollectionFactory = $bannersCollectionFactory; 
+        $this->bannerSlideCollectionFactory = $bannerSlideCollectionFactory; 
         // For testing use
         $this->bannersFactory = $bannersFactory->create();
         $this->dataPersistor = $dataPersistor;
@@ -68,7 +71,7 @@ class BannerDataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider {
         $items = $this->collection->getItems();
                 
         // This does not make sense at first, but it seems that $this->collection->getItems() get's a single item from the collection based on the requestFieldName
-        // try throw new \Exception( count($items) ) and this will only show one item. so looks like this is the way to go
+        // try throw new \Exception( count($items) ) and this will show one item. so looks like this is the way to go
         foreach ($items as $banner) {
             // $banner->getId() will automatically map to the idfield based on what is defined in it's resource model, no need to use getBannerId(). ?
             $this->loadedData[$banner->getId()] = $banner->getBannerData();
@@ -85,16 +88,14 @@ class BannerDataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider {
         
         // add slides
         if( isset($this->loadedData[ $banner_id ] ) ){
-            $collectionFactory = $this->bannersCollectionFactory->create();
-            $collectionFactory->getBannerSlides( $banner_id );
-            
-            ob_start();
-            foreach($collectionFactory as $i){
-                var_dump($i->getData());
+            $bannerSlideRelation = $this->bannerSlideCollectionFactory->create();
+            $bannerSlideRelation->getBannerSlides( $banner_id );
+            //throw new \Exception($collectionFactory->getBannerSlides( $banner_id )); will actually display the sql query
+
+            foreach($bannerSlideRelation as $i){
+                $this->loadedData[ $banner_id ]['slides']['slides_listing'][] = $i->getData();
             }
-            $dump = ob_get_clean();
-            throw new \Exception( $dump ) ;
-            //throw new \Exception($collectionFactory->getBannerSlides( $banner_id ));
+            
         }
 
         // Dummy data format
@@ -117,7 +118,7 @@ class BannerDataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider {
     }
 
     /**
-     * Be sure to implement this fuction in order to trigger the AddSlides modifier
+     * Be sure to implement this fuction as is, in order to trigger the AddSlides modifier
      * see Prymag\BannerSlier\Ui\DataProvider\Banners\Form\Modifiers\AddSlides.php
      *
      * @return void

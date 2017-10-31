35da9d46fa53530a7eb0fd6adcf48fa71d3b6079
Perry <perry.mark@zeald.com>
2017-10-31 12:33:46 +0800

    cleanup

--- Model/ResourceModel/Banners.php
+++ Model/ResourceModel/Banners.php
@@ -4,15 +4,6 @@ namespace Prymag\BannerSlider\Model\ResourceModel;
 
 class Banners extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb {
     
-    protected $bannerSliderRelation;
-
-    public function __construct(
-        \Magento\Framework\Model\ResourceModel\Db\Context $context
-    ){
-        parent::__construct($context);
-        $this->bannerSliderTable = $this->getTable('prymag_banner_slider_relation');
-    }
-
     /**
      * Define table for this resource and the primary key
      *
